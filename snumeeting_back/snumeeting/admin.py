cc74f7830d77010a56f69820ffbb284d0b5b4e87
seungchan <matues00@snu.ac.kr>
2017-10-27 22:01:44 +0900

    created api models

---
+++ snumeeting_back/snumeeting/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Ex_User, Meeting, Comment, Subject, College
+
+admin.site.register(Ex_User)
+admin.site.register(Meeting)
+admin.site.register(Comment)
+admin.site.register(Subject)
+admin.site.register(College)
---
+++ snumeeting_back/snumeeting/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class College(models.Model):
+  name = models.CharField(max_length=64)
+
+class Ex_User(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  college = models.ForeignKey(
+    College,
+    related_name = 'users',
+    null=True
+  )
+
+class Subject(models.Model):
+  interest = models.CharField(max_length=64)
+  name = models.CharField(max_length=64)
+  users = models.ManyToManyField(
+    Ex_User,
+    related_name = 'subjects',
+  )
+
+class Meeting(models.Model):
+  author = models.ForeignKey(
+    Ex_User,
+    related_name='meetingsAuthor',
+    null=True
+  )
+  description = models.TextField()
+  location = models.CharField(max_length=64)
+  max_member = models.IntegerField()
+  members = models.ManyToManyField(
+    Ex_User,
+    related_name = 'meetingsMembers',
+  )
+  subject = models.ForeignKey(
+    Subject,
+    related_name = 'meetingsSubject',
+    null=True
+  )
+
+class Comment(models.Model):
+  author = models.ForeignKey(
+    Ex_User,
+    related_name='commentsAuthor',
+    null=True
+  )
+  meeting = models.ForeignKey(
+    Meeting,
+    related_name='commentsMeeting',
+    null=True
+  )
+  content = models.CharField(max_length=64)
+  publicity = models.BooleanField()
