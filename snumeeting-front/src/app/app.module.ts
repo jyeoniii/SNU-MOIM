255b16e6e9fbdf6825c8d147deceaad12da6c280
93lemondrops <93lemondrops@gmail.com>
2017-11-04 01:02:15 +0900

    Added components for user activities

--- snumeeting-front/src/app/app.module.ts
+++ snumeeting-front/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    ProfileComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule
