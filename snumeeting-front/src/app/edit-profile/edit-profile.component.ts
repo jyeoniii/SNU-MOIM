edd0f55d9ab2971f34afe3259cb45a2ea91b483e
93lemondrops <93lemondrops@gmail.com>
2017-11-05 17:24:24 +0900

    Implemented sign up & edit profile

--- snumeeting-front/src/app/edit-profile/edit-profile.component.ts
+++ snumeeting-front/src/app/edit-profile/edit-profile.component.ts
@@ -24,6 +24,9 @@ export class EditProfileComponent implements OnInit {
   user: User;
   colleges: College[];
   subjects: Subject[];
+  interests: string[];
+  interestChecked = [];
+  subjectChecked = [];
 
   ngOnInit() {
     this.route.paramMap
@@ -31,9 +34,46 @@ export class EditProfileComponent implements OnInit {
       .subscribe(user => this.user = user);
 
     this.userService.getCollegeList().then(colleges => this.colleges = colleges);
+    this.userService.getSubjectList().then(subjects => this.subjects = subjects);
+    this.userService.getInterestList().then(interests => this.interests = interests);
   }
 
-  editProfile() {
-    console.log('edit profile');
+  interestCheck(interest: string) {
+    if (this.interestChecked[interest]) {
+      this.interestChecked[interest] = false;
+    } else {
+      this.interestChecked[interest] = true;
+    }
+  }
+
+  subjectCheck(subjectName: string) {
+    if (this.subjectChecked[subjectName]) {
+      this.subjectChecked[subjectName] = false;
+    } else {
+      this.subjectChecked[subjectName] = true;
+    }
+  }
+
+  editProfile(password: string, passwordCheck: string, name: string) {
+    if (password === passwordCheck) {
+      var selectedSubjects: Subject[] = [];
+
+      for (let subject of this.subjects) {
+        if (this.subjectChecked[subject.name]) {
+          selectedSubjects.push(subject);
+        }
+      }
+
+      this.user.password = password;
+      this.user.name = name;
+      this.user.subjects = selectedSubjects;
+
+      this.userService.editUserInfo(this.user).then(() => {
+        alert('Edit Profile Success!')
+        this.router.navigate(['/user', this.user.id]);
+      });
+    } else {
+      alert('Password and Check doesn\'t match.');
+    }
   }
 }
--- snumeeting-front/src/app/sign-up/sign-up.component.ts
+++ snumeeting-front/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { User } from '../user';
 import { College } from '../college';
@@ -14,17 +15,63 @@ import { UserService } from '../user-service';
 export class SignUpComponent implements OnInit {
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) { }
+
+  selectedCollege: College;
+  colleges: College[];
+  subjects: Subject[];
+  interests: string[];
+  interestChecked = [];
+  subjectChecked = [];
 
   ngOnInit() {
     this.userService.getCollegeList().then(colleges => this.colleges = colleges);
+    this.userService.getSubjectList().then(subjects => this.subjects = subjects);
+    this.userService.getInterestList().then(interests => this.interests = interests);
   }
 
-  colleges: College[];
-  subjects: Subject[];
-
-  signUp() {
-    console.log('sign up');
+  interestCheck(interest: string) {
+    if (this.interestChecked[interest]) {
+      this.interestChecked[interest] = false;
+    } else {
+      this.interestChecked[interest] = true;
+    }
   }
+
+  subjectCheck(subjectName: string) {
+    if (this.subjectChecked[subjectName]) {
+      this.subjectChecked[subjectName] = false;
+    } else {
+      this.subjectChecked[subjectName] = true;
+    }
+  }
+
+  signUp(username: string, password: string, passwordCheck: string, name: string) {
+    if (password === passwordCheck) {
+      var selectedSubjects: Subject[] = [];
+
+      for (let subject of this.subjects) {
+        if (this.subjectChecked[subject.name]) {
+          selectedSubjects.push(subject);
+        }
+      }
+
+      var newUser = new User();
+      newUser.username = username;
+      newUser.password = password;
+      newUser.name = name;
+      newUser.college = this.selectedCollege;
+      newUser.subjects = selectedSubjects;
+
+      this.userService.signUp(newUser).then(() => {
+        alert('Sign Up Success!')
+        this.router.navigate(['/sign_in']);
+      });
+  } else {
+    alert('Password and Check doesn\'t match.');
+    }
+  }
+
 }
