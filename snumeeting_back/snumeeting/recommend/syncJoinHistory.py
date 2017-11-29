41241ece838b3de0bd1b5d3450cefc0d5acbae2a
jyeoniii <jaykim9438@gmail.com>
2017-11-29 23:55:26 +0900

    Similarity measure
    - Add joinHistory field to Ex_User, College model
    - Add operations to handle joinHistory
    - Add similarity measuring algorithm

---
+++ snumeeting_back/snumeeting/recommend/syncJoinHistory.py
@@ -0,0 +1,40 @@
+from snumeeting.models import Ex_User, College
+from .JoinHistoryManager import JoinHistoryManager
+import json
+
+def SyncUserHistoryAll():
+  users = Ex_User.objects.all()
+  for user in users:
+    ReinitUserHistory(user)
+
+  print("SyncUserHistoryAll() done")
+
+def SyncCollegeHistoryAll():
+  colleges = College.objects.all()
+  for college in colleges:
+    ReinitCollegeHistory(college)
+
+  print("SyncCollegeHistoryAll() done")
+
+def ReinitUserHistory(user):
+  manager = JoinHistoryManager()
+  joinedMeetings = user.meetings_joined.all()
+  jh = "{}"
+  for meeting in joinedMeetings:
+    subject_id = meeting.subject_id
+    jh = manager.increaseCnt(jh, subject_id)
+  user.joinHistory = jh
+  user.save()
+
+def ReinitCollegeHistory(college):
+  manager = JoinHistoryManager()
+  collegeMembers = college.users.all()
+  jh = "{}"
+  for user in collegeMembers:  # Iterate every college members
+    joinedMeetings = user.meetings_joined.all()
+    for meeting in joinedMeetings:
+      subject_id = meeting.subject_id
+      jh = manager.increaseCnt(jh, subject_id)
+  college.joinHistory = jh
+  college.save()
+
