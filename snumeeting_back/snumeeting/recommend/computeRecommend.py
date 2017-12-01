ea8516876fef03ec6f539e19eec704b581aa1fd1
jyeoniii <jaykim9438@gmail.com>
2017-12-01 22:07:52 +0900

    Recommend meeting (User-based filtering)

--- snumeeting_back/snumeeting/recommend/computeRecommend.py
+++ snumeeting_back/snumeeting/recommend/computeRecommend.py
@@ -2,12 +2,38 @@ from snumeeting.models import Ex_User, College
 from .similaritymeasures import Similarity 
 from .JoinHistoryManager import JoinHistoryManager
 import heapq
+import random
 from operator import itemgetter
 
-def getUserSimilarity(target):
-  # Find the user having the most similar join history
+def getRecMeetingsUserBased(target, N):
+  # Get meetings that similar users is joined
+  # N : maximum number of meetings to be recommended
 
-  K = 5   # number of similar users to be recommended
+  K = 3     # number of similar users to be recommended
+  similar_users = dict(getUserSimilarity(target,K))
+  result = []
+
+  sim_user_ids = list(similar_users.keys())
+  for uid in sim_user_ids:
+    user = Ex_User.objects.get(id=uid)
+    meetings_joined = list(user.meetings_joined.all())
+    result += meetings_joined
+
+  if len(result) > N:
+    result = result[:N]
+
+  result = list(filter(
+      (lambda m: not (m.is_closed or (target in m.members.all()))),
+       result))
+
+  return result
+
+def getUserSimilarity(target, K):
+  # Find the user having the most similar join history
+  # target : target user to calculte similarity
+  # K : number of similar users to be recommended
+  # return: List of tuples - [ top K (Ex_User's id, similarity score) ]
+
   measure = Similarity()
   manager = JoinHistoryManager()
   users = Ex_User.objects.all()
@@ -16,7 +42,6 @@ def getUserSimilarity(target):
   jh_target = manager.convertToList(target.joinHistory)
 
   d_colleges= getCollegeDistance(target.college)
-  print(d_colleges)
 
   for user in users:
     if user.id == target.id:
@@ -27,7 +52,7 @@ def getUserSimilarity(target):
 
     # Convert distance to similarity
     # (Current weight) InterestDistance:CollegeDistance = 2:1
-    print("id:"+str(user.id)+" interest distance: "+str(jh_d)+ " / college distance: " + str(col_d/2))
+    # print("id:"+str(user.id)+" interest distance: "+str(jh_d)+ " / college distance: " + str(col_d/2))
     similarities[user.id] = 1 / (1 + jh_d + col_d/2)  
 
   return heapq.nlargest(K, similarities.items(), key=itemgetter(1)) 
