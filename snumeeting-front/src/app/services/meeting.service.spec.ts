d879fa7360da3a6f410fc798a9384b9f26248edb
jyeoniii <jaykim9438@gmail.com>
2017-12-18 17:31:54 +0900

    Add testing

--- snumeeting-front/src/app/services/meeting.service.spec.ts
+++ snumeeting-front/src/app/services/meeting.service.spec.ts
@@ -7,8 +7,10 @@ import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { MeetingService } from './meeting.service';
 import { Meeting } from '../models/meeting';
+import { User } from '../models/user';
+import { Tag } from '../models/tag';
 
-import { makeMeetingData } from '../models/mock-data';
+import { makeMeetingData, makeUserData, makeTagData } from '../models/mock-data';
 
 describe('MeetingService', () => {
   beforeEach(async(() => {
@@ -96,7 +98,20 @@ describe('MeetingService', () => {
       })));
   });
 
-  describe('when editMeeting', () => {
+  describe('when editMeeting / delete meeting', () => {
+    let backend: MockBackend;
+    let fakeMeeting: Meeting[];
+    let service: MeetingService;
+    let response: Response;
+
+    beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
+      backend = be;
+      service = new MeetingService(http);
+      fakeMeeting = makeMeetingData();
+      const options = new ResponseOptions({status: 200, body: fakeMeeting});
+      response = new Response(options);
+    }));
+
     it('should successfully update backend data', async(inject([MeetingService], (service: MeetingService) => {
       service.getMeeting(3)
         .then(meeting => {
@@ -108,9 +123,6 @@ describe('MeetingService', () => {
         });
     })));
 
-  });
-
-  describe('when deleteMeeting', () => {
     it('should successfully delete selected meeting on backend', async(inject([MeetingService], (service: MeetingService) => {
       service.deleteMeeting(2)
         .then(deletedMeeting => {
@@ -120,6 +132,170 @@ describe('MeetingService', () => {
     })));
 
   });
+
+  describe('when joinMeeting / leaveMeeting / closeMeeting', () => {
+    let backend: MockBackend;
+    let fakeMeeting: Meeting[];
+    let service: MeetingService;
+    let response: Response;
+
+    beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
+      backend = be;
+      service = new MeetingService(http);
+      fakeMeeting = makeMeetingData();
+      const options = new ResponseOptions({status: 200, body: fakeMeeting});
+      response = new Response(options);
+    }));
+
+    it('joinMeeting should successfully update members of the meeting', async(inject([MeetingService], (service: MeetingService) => {
+      const target_meeting = fakeMeeting[0];
+      service.joinMeeting(target_meeting.id, 2)
+        .then(() => {
+          service.getMeeting(target_meeting.id).then(updated => {
+            backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+            expect(updated.members.length).toBe(target_meeting.members.length + 1);
+          });
+        });
+    })));
+
+    it('leaveMeeting should successfully update members of the meeting', async(inject([MeetingService], (service: MeetingService) => {
+      const target_meeting = fakeMeeting[0];
+      service.leaveMeeting(target_meeting.id, 2)
+        .then(() => {
+          service.getMeeting(target_meeting.id).then(updated => {
+            backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+            expect(updated.members.length).toBe(target_meeting.members.length - 1);
+          });
+        });
+    })));
+
+    it('closeMeeting should successfully update is_closed', async(inject([MeetingService], (service: MeetingService) => {
+      const target_meeting = fakeMeeting[0];
+      service.closeMeeting(target_meeting.id)
+        .then(() => {
+          service.getMeeting(target_meeting.id).then(updated => {
+            backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+            expect(updated.is_closed).toBe(true);
+          });
+        });
+    })));
+
+  });
+
+  describe('when getJoinedMeeting', () => {
+    let backend: MockBackend;
+    let fakeUser: User[];
+    let fakeMeeting: Meeting[];
+    let service: MeetingService;
+    let response: Response;
+
+    beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
+      backend = be;
+      service = new MeetingService(http);
+      fakeUser = makeUserData();
+      fakeMeeting = makeMeetingData();
+      const options = new ResponseOptions({status: 200, body: fakeMeeting});
+      response = new Response(options);
+    }));
+
+    it('should return expected meetings', async(inject([MeetingService], (service: MeetingService) => {
+      service.getJoinedMeeting(2)
+        .then(meetings => {
+          backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+          expect(meetings.length).toBe(2);
+        });
+    })));
+
+  });
+
+  describe('when getMeetingsOnTag', () => {
+    let backend: MockBackend;
+    let fakeTag: Tag[];
+    let service: MeetingService;
+    let response: Response;
+
+    beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
+      backend = be;
+      service = new MeetingService(http);
+      fakeTag = makeTagData();
+    }));
+
+    it('should get meetings related to the tag', async(inject([MeetingService], (service: MeetingService) => {
+      const options = new ResponseOptions({status: 200, body: fakeTag});
+      response = new Response(options);
+      backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+      service.getMeetingsOnTag('study')
+        .then(meetings => {
+          expect(meetings.length).toBe(2);
+        });
+    })));
+
+    // it('should return empty array when the tag does not exists', async(inject([MeetingService], (service: MeetingService) => {
+    //   const options = new ResponseOptions({status: 404, body: fakeTag});
+    //   response = new Response(options);
+    //   backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+    //   service.getMeetingsOnTag('nonexisting')
+    //     .then(response => {
+    //       expect(response).toBe(0);
+    //     });
+    // })));
+
+  });
+
+  describe('when test searching', () => {
+    let backend: MockBackend;
+    let fakeUser: User[];
+    let fakeMeeting: Meeting[];
+    let service: MeetingService;
+    let response: Response;
+
+    beforeEach(inject([Http, XHRBackend], (http: Http, be: MockBackend) => {
+      backend = be;
+      service = new MeetingService(http);
+      fakeUser = makeUserData();
+      fakeMeeting = makeMeetingData();
+      const options = new ResponseOptions({status: 200, body: fakeMeeting});
+      response = new Response(options);
+    }));
+
+    it('searchMeetingsOfTitle should return expected meetings', async(inject([MeetingService],
+      (service: MeetingService) => {
+        service.searchMeetingsOfTitle('1')
+          .then(meetings => {
+            backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+            expect(meetings.length).toBe(1);
+          });
+      })));
+
+    it('searchMeetingsOfAuthor should return expected meetings', async(inject([MeetingService],
+      (service: MeetingService) => {
+        service.searchMeetingsOfAuthor('2')
+          .then(meetings => {
+            backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+            expect(meetings.length).toBe(1);
+          });
+      })));
+
+    it('searchMeetingsOfSubject should return expected meetings(subject id only)', async(inject([MeetingService],
+      (service: MeetingService) => {
+        service.searchMeetingsOfSubject('3')
+          .then(meetings => {
+            backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+            expect(meetings.length).toBe(2);
+          });
+      })));
+
+    it('searchMeetingsOfSubject should return expected meetings(subject id and title)', async(inject([MeetingService],
+      (service: MeetingService) => {
+        service.searchMeetingsOfSubject('3_an')
+          .then(meetings => {
+            backend.connections.subscribe((c: MockConnection) => c.mockRespond(response));
+            expect(meetings.length).toBe(1);
+          });
+      })));
+  });
+
+
 });
 
 
